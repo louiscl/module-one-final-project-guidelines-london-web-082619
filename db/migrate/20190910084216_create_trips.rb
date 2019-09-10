a573e19466672272fdc54a4a3bf73da1fd062300
Abdullah Bajaman <ambajaman@gmail.com>
2019-09-10 10:08:46 +0100

    created trips and users tables

--- db/migrate/20190910084216_create_trips.rb
+++ db/migrate/20190910084216_create_trips.rb
@@ -1,4 +1,10 @@
 class CreateTrips < ActiveRecord::Migration[5.0]
   def change
+    create_table :trips do |t|
+      t.integer :flight_id
+      t.integer :user_id
+
+      t.timestamp
+    end
   end
 end
