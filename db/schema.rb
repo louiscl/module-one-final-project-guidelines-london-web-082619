8186f4be7a5a570f46a8567f7402bd1cf8a0e4e9
github username <github email address>
2019-09-10 10:16:23 +0100

    created migrations for flight and airline

---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20190910090414) do
+
+  create_table "airlines", force: :cascade do |t|
+    t.string "name"
+    t.string "business_model"
+    t.date   "founded"
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.string   "origin"
+    t.string   "destination"
+    t.datetime "date_time"
+    t.integer  "price"
+    t.integer  "airline_id"
+  end
+
+end
