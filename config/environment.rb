b82b421580b5b079f6283d69f670b052f3cdd1bf
github username <github email address>
2019-09-11 16:54:01 +0100

    active record silence m

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,6 @@ Bundler.require
 require 'colorize'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger = nil
+
 require_all 'app'
